b0ecac88725fd432285e0067ff9d0d5d9f6f4a42
metehanugus <65510727+metehanugus@users.noreply.github.com>
2023-10-18 20:53:31 +0300

    Bugfix
    small bugfix app.js

--- static/app.js
+++ static/app.js
@@ -38,6 +38,6 @@ function createColorBoxes(colors, parent) {
         const span = document.createElement("span");
         span.innerText = color;
         div.appendChild(span);
-        container.appendChild(div);      
+        parent.appendChild(div);      
     }
 }
